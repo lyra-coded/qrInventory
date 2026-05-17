c2d6a2e69183df1189f0e1f78faa4c49e74340ac
Lyra <wsmeorc.b2@gmail.com>
2026-05-17 13:20:51 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+## Goal
+# generate QR code when requested, tie code to unique ID in database
+# remove QR code from database
+# 
+
+# import QR code library
+
+
+## put
+
+## get
+
+## post
+# new QR code (and bike) to the database, return QR Code that is attached to new unique ID 
+
+## delete
+# remove QR code tie with unique ID
