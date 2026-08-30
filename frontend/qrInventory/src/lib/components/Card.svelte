45b43b093e4f13a7feef9e65216ae243254aee7b
lyra <wsmeorc.b2@gmail.com>
2026-08-30 19:49:07 -0400

    added card component

---
+++ frontend/qrInventory/src/lib/components/Card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    let { title, body = "????"} = $props();
+</script>
+
+<div class="card">
+    <h3>{title}</h3>
+    <strong>{body}</strong>
+</div>
+
+<style>
+    .card {
+        background: #fff;
+        border: 1px solid #eee;
+        border-radius: 18px;
+        box-shadow:
+            0 2px 4px rgba(0, 0, 0, 0.08),
+            0 3px 8px rgba(0, 0, 0, 0.08);
+        padding: 20px 24px;
+    }
+
+    .card-title {
+        color: #557da3;
+        font-size: 17px;
+        font-weight: 400;
+    }
+</style>
--- frontend/qrInventory/src/routes/dashboard/+page.svelte
+++ frontend/qrInventory/src/routes/dashboard/+page.svelte
@@ -1,2 +1,24 @@
+<script>
+    import Card from "$lib/components/Card.svelte";
+
+    let numBikesDonatedToday = 54;
+</script>
+
 <h1>Dashboard</h1>
 <p>Your inventory tables and data go here.</p>
+
+<div class="flex-container">
+    <Card title={"Total Bikes"} />
+    <Card title={"Donated Today"} body={numBikesDonatedToday}/>
+</div>
+
+<style>
+    .flex-container{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem;
+    }
+</style>
